841246c235ff3917d3916611f49420a3d04a28b2
EvstifeevaKristina <94647555+EvstifeevaKristina@users.noreply.github.com>
2022-03-20 15:20:46 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,60 @@
 
 
 double pown(double value, uint16_t n) {
-
+ if (n == 0) {
+        return 1;
+    }
+    if (n == 1) {
+        return value;
+    } else {
+        return value * (pown(value, n - 1));
+    }
 }
+  
 
 uint64_t fact(uint16_t n) {
-
+ if (n <= 1) {
+        return 1;
+    } else {
+        return n * fact(n - 1);
+    }
 }
 
 double calcItem(double x, uint16_t n) {
-
+ return (pown(x, n) / fact(n));
 }
 
 double expn(double x, uint16_t count) {
+ double result_ex = 1;
+    for (int n = count; n > 0; n--) {
+        result_ex += pown(x, n) / fact(n);
+    }
+    return result_ex;
 
 }
 
 double sinn(double x, uint16_t count) {
+  if (count == 1) {
+        return x;
+    }
+    double result_sin = 0;
+    for (int n = count; n > 0; n--) {
+        result_sin += (pown((-1), n - 1))*((pown(x, 2 * n - 1))
+            /(fact(2 * n - 1)));
+    }
+    return result_sin;
 
 }
 
 double cosn(double x, uint16_t count) {
+ double result_cos = 0;
+    if (count == 1) {
+        return x;
+    }
+    for (int n = count; n > 0; n--) {
+        result_cos += (pown((-1), n - 1))*((pown(x, 2 * n - 2))
+            /(fact(2 * n - 2)));
+    }
+    return result_cos;
 
 }
